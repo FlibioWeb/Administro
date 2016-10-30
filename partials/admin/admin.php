f1b7c894b2d475929e08afbad4820df4f73bd428
Flibio <flibio.business@gmail.com>
2016-10-30 08:29:42 -0600

    Add style to admin panel

--- partials/admin/admin.php
+++ partials/admin/admin.php
@@ -16,15 +16,41 @@
         die("Redirecting...");
     }
 
+    $user = $usermanager->getUser()["display"];
     $siteName = $administro->configmanager->getConfiguration()["name"];
 ?>
 <html>
-    <title><?php echo $siteName ?> Administration</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo BASEPATH; ?>partials/admin/style/main.css">
+    <title>Admin | <?php echo $siteName ?></title>
+    <link rel="stylesheet" type="text/css" href="<?php echo BASEPATH; ?>partials/style/admin.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
 </html>
 
 <body>
-    <h2><?php echo $siteName ?> Administration</h2>
-    <p>
-
+    <aside class="sidebar">
+        <header class="header">
+            Administration Panel
+        </header>
+        <section class="user">
+            Welcome, <?php echo $user; ?>
+            <p>
+            <a href="<?php echo BASEPATH; ?>logout"><i class="fa fa-sign-out"></i> Logout</a>
+        </section>
+        <nav class="nav">
+            <ul>
+                <li><a href="<?php echo BASEPATH ?>admin"><i class="fa fa-home"></i> Home</a></li>
+                <li><a href="<?php echo BASEPATH ?>admin/pages"><i class="fa fa-file-text"></i> Pages</a></li>
+            </ul>
+        </nav>
+        <footer class="footer">
+            <a href="<?php echo BASEPATH; ?>"><i class="fa fa-angle-left"></i> Back to Site</a>
+        </footer>
+    </aside>
+    <main class="main">
+        <header class="header">
+            Home
+        </header>
+        <main class="board">
+            Control Panel
+        </main>
+    </main>
 </body>
--- partials/login.php
+++ partials/login.php
@@ -3,11 +3,32 @@
     use \Administro\Administro;
     use \Administro\Form\FormUtils;
 
+    $administro = Administro::Instance();
+    $title = $administro->configmanager->getConfiguration()["name"];
+
+    if($administro->usermanager->isLoggedIn()) {
+        // The user is already logged in
+        $_SESSION["message-bad"] = "You are already logged in!";
+        header("Location: ".BASEPATH);
+        die("Redirecting...");
+    }
+
+    // Create messages
+    $good = (isset($_SESSION["message-good"]) ? $_SESSION["message-good"] : "");
+    unset($_SESSION["message-good"]);
+    $bad = (isset($_SESSION["message-bad"]) ? $_SESSION["message-bad"] : "");
+    unset($_SESSION["message-bad"]);
+
     // Generate a form token
     $token = FormUtils::generateToken("login");
 ?>
-
+<html>
+    <title>Login | <?php echo $title; ?></title>
+    <link rel="stylesheet" type="text/css" href="<?php echo BASEPATH; ?>partials/style/main.css">
+</html>
 <body>
+    <h3><?php echo $title; ?> Login</h3>
+    <p>
     <form method="post" action="<?php echo BASEPATH."form/"; ?>login">
         <input type="hidden" name="token" value="<?php echo $token; ?>">
         <label>Username</label><p><input type="text" name="username">
@@ -16,4 +37,8 @@
         <p>
         <input type="submit" value="Login">
     </form>
+    <p>
+    <span class="good"><?php echo $good; ?></span>
+    <p>
+    <span class="bad"><?php echo $bad; ?></span>
 </body>
